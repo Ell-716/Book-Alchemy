6582da9d1d9e383f8c483b503eb79125810a4230
Ell-716 <elenabai.2021@gmail.com>
2024-11-25 13:50:58 +0100

    Add book route.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from data_models import db, Author, Book
 import os
@@ -9,7 +9,6 @@ app = Flask(__name__)
 base_dir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{base_dir}/data/library.sqlite"
 
-
 db.init_app(app)
 
 # Create the database tables. Run once
@@ -19,15 +18,14 @@ db.init_app(app)
 
 @app.route("/add_author", methods=["GET", "POST"])
 def add_author():
-
     if request.method == "POST":
         name = request.form.get('name').strip()
         birth_date = request.form.get('birth_date').strip()
         date_of_death = request.form.get('date_of_death').strip()
 
         if not name:  # Ensure the name is not empty
-            success_message = "Author name is required."
-            return redirect(url_for('add_author', success_message=success_message))
+            warning_message = "Author name is required."
+            return render_template("add_author.html", warning_message=warning_message)
 
         author = Author(
             name=name,
@@ -39,9 +37,40 @@ def add_author():
         db.session.commit()
 
         success_message = "Author added successfully!"
-
-        # Redirect to the same page with success message in the URL
-        return redirect(url_for('add_author', success_message=success_message))
+        return render_template("add_author.html", success_message=success_message)
 
     if request.method == "GET":
         return render_template("add_author.html")
+
+
+@app.route("/add_book", methods=["GET", "POST"])
+def add_book():
+    if request.method == "POST":
+        isbn = request.form.get('isbn').strip()
+        title = request.form.get('title').strip()
+        publication_year = request.form.get('publication_year').strip()
+        author_id = request.form.get('author_id')
+
+        if not title:
+            warning_message = "Book title is required."
+            return render_template("add_book.html",
+                                   authors=Author.query.all(),
+                                   warning_message=warning_message)
+
+        book = Book(
+            author_id=author_id,
+            isbn=isbn,
+            title=title,
+            publication_year=publication_year if publication_year else None
+        )
+
+        db.session.add(book)
+        db.session.commit()
+
+        success_message = "Book added successfully!"
+        return render_template("add_book.html",
+                               authors=Author.query.all(),
+                               success_message=success_message)
+
+    if request.method == "GET":
+        return render_template("add_book.html", authors=Author.query.all())
