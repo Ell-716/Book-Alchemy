edcf7603316c77093bbd44abc260817c6ac0a9ae
Ell-716 <elenabai.2021@gmail.com>
2024-11-25 13:14:06 +0100

    Add author route.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from data_models import db, Author, Book
 import os
@@ -13,5 +13,35 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{base_dir}/data/library.sqli
 db.init_app(app)
 
 # Create the database tables. Run once
-#with app.app_context():
-    #db.create_all()
+# with app.app_context():
+#     db.create_all()
+
+
+@app.route("/add_author", methods=["GET", "POST"])
+def add_author():
+
+    if request.method == "POST":
+        name = request.form.get('name').strip()
+        birth_date = request.form.get('birth_date').strip()
+        date_of_death = request.form.get('date_of_death').strip()
+
+        if not name:  # Ensure the name is not empty
+            success_message = "Author name is required."
+            return redirect(url_for('add_author', success_message=success_message))
+
+        author = Author(
+            name=name,
+            birth_date=birth_date if birth_date else None,
+            date_of_death=date_of_death if date_of_death else None
+        )
+
+        db.session.add(author)
+        db.session.commit()
+
+        success_message = "Author added successfully!"
+
+        # Redirect to the same page with success message in the URL
+        return redirect(url_for('add_author', success_message=success_message))
+
+    if request.method == "GET":
+        return render_template("add_author.html")
