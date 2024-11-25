32239d8b08399e1ed3e9e83dfdb1b41ecc8b42b8
Ell-716 <elenabai.2021@gmail.com>
2024-11-25 12:29:55 +0100

    Define the Author and Book models.

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from data_models import db, Author, Book
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
+# Get the absolute path to the current directory
+base_dir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{base_dir}/data/library.sqlite"
+
 
 db.init_app(app)
+
+# Create the database tables. Run once
+#with app.app_context():
+    #db.create_all()
--- data_models.py
+++ data_models.py
@@ -1,3 +1,37 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
 
 db = SQLAlchemy()
+
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    birth_date = Column(Date, nullable=True)
+    date_of_death = Column(Date, nullable=True)
+
+    def __repr__(self):
+        return f"Author(id = {self.id}, name = {self.name})"
+
+    def __str__(self):
+        return f"{self.id}. {self.name} ({self.birth_date} - {self.date_of_death})"
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    isbn = Column(String, nullable=False, unique=True)
+    title = Column(String, nullable=False)
+    publication_year = Column(Integer, nullable=True)
+    author_id = Column(Integer, ForeignKey('authors.id'), nullable=False)
+
+    def __repr__(self):
+        return (f"Book(id = {self.id}, isbn = {self.isbn}, title = {self.title}, "
+                f"publication_year = {self.publication_year})")
+
+    def __str__(self):
+        return f"{self.id}. {self.title} ({self.publication_year})"
+
